c7d41de4f177878f24c976062ed8077bb09641d9
rawlingsj <rawlingsj80@gmail.com>
2018-01-21 21:45:37 +0000

    Initial import

---
+++ src/main/java/com/example/mydemo1/MyDemo1Application.java
@@ -0,0 +1,12 @@
+package com.example.mydemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDemo1Application.class, args);
+	}
+}
